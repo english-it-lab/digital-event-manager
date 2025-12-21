4d56eaec00d33de667810aa57c3a3d3f4af465f4
WorkManOne <arkhipov.ki@yandex.ru>
2025-12-21 20:48:36 +0400

    Change generation approach to single folder

--- backend/generate_api.bat
+++ backend/generate_api.bat
@@ -1,21 +1,19 @@
 @echo off
-setlocal enabledelayedexpansion
 
-call .venv\Scripts\activate.bat
+if exist .venv\Scripts\activate.bat (
+    call .venv\Scripts\activate.bat
+)
+
 pip install openapi-generator-cli
 
 for %%f in (resources\api\v1\*.yaml) do (
-    set "file=%%~nf"
-    
-    openapi-generator-cli generate -i %%f -g python-fastapi -o generated_!file!\
-    
-    if exist openapi_server_!file!\ rd /s /q openapi_server_!file!\
-    
-    xcopy generated_!file!\src\openapi_server openapi_server_!file!\ /e /i /q
-    
-    findstr /v "uvloop==0.21.0" generated_!file!\requirements.txt > openapi_server_!file!\requirements.txt
-    
-    pip install -r openapi_server_!file!\requirements.txt
-    
-    rd /s /q generated_!file!\
+    openapi-generator-cli generate -i %%f -g python-fastapi -o generated\
 )
+
+if exist openapi_server\ rd /s /q openapi_server
+
+xcopy generated\src\openapi_server openapi_server\ /e /i /q
+findstr /v "uvloop==0.21.0" generated\requirements.txt > openapi_server\requirements.txt
+pip install -r openapi_server\requirements.txt
+
+rd /s /q generated
--- backend/generate_api.sh
+++ backend/generate_api.sh
@@ -1,28 +1,27 @@
 #!/bin/sh
 set -e
 
-source .venv/Scripts/activate
+if [ -f ".venv/bin/activate" ]; then
+    . .venv/bin/activate
+elif [ -f ".venv/Scripts/activate" ]; then
+    . .venv/Scripts/activate
+fi
+
 pip install openapi-generator-cli
 
 for yaml_file in resources/api/v1/*.yaml; do
-    file=$(basename "$yaml_file" .yaml)
-    
     openapi-generator-cli generate \
         -i "$yaml_file" \
         -g python-fastapi \
-        -o generated_${file}/
-    
-    if [ -d "openapi_server_${file}" ]; then
-        rm -rf openapi_server_${file}
-    fi
-    
-    cp -r generated_${file}/src/openapi_server openapi_server_${file}
-    
-    grep -v "uvloop==0.21.0" generated_${file}/requirements.txt > generated_${file}/requirements_clean.txt
-    mv generated_${file}/requirements_clean.txt generated_${file}/requirements.txt
-    cp generated_${file}/requirements.txt openapi_server_${file}/requirements.txt
-    
-    pip install -r "openapi_server_${file}/requirements.txt"
-    
-    rm -rf generated_${file}
+        -o generated/
 done
+
+if [ -d "openapi_server" ]; then
+    rm -rf openapi_server
+fi
+
+cp -r generated/src/openapi_server openapi_server
+grep -v "uvloop==0.21.0" generated/requirements.txt > openapi_server/requirements.txt
+pip install -r openapi_server/requirements.txt
+
+rm -rf generated
