334be4ae133f01336f3b947c997cd5808ed21a44
WorkManOne <arkhipov.ki@yandex.ru>
2026-05-10 12:16:18 +0400

    Add router and adapter

--- backend/app/adapters/api/v1/__init__.py
+++ backend/app/adapters/api/v1/__init__.py
@@ -17,9 +17,11 @@ from app.adapters.api.v1 import (
 )
 from app.adapters.api.v1.draw import DrawApiImpl
 from app.adapters.api.v1.draw_results import DrawResultsApiImpl
+from app.adapters.api.v1.notifications import NotificationsApiImpl
 from app.adapters.api.v1.topics import TopicsApiImpl
 from openapi_server.apis.draw_api import router as draw_router
 from openapi_server.apis.draw_results_api import router as draw_results_router
+from openapi_server.apis.notifications_api import router as notifications_router
 from openapi_server.apis.topics_api import router as topics_router
 
 router = APIRouter()
@@ -37,4 +39,5 @@ router.include_router(section_juries.router, prefix="/section-juries")
 router.include_router(groups.router, prefix="/groups")
 router.include_router(draw_router)
 router.include_router(draw_results_router)
+router.include_router(notifications_router)
 router.include_router(topics_router)
