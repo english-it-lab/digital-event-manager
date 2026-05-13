2e52d1157c05878d8050b4941f33bdcda20762ee
sour-soup <kiziyepi8@gmail.com>
2026-05-14 02:37:28 +0400

    cosmetic change

--- backend/app/repositories/topic.py
+++ backend/app/repositories/topic.py
@@ -1,7 +1,7 @@
-from sqlalchemy import select
-from sqlalchemy.ext.asyncio import AsyncSession
 from openapi_server.models.topic_create import TopicCreate
 from openapi_server.models.topic_update import TopicUpdate
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models import Topic
 
