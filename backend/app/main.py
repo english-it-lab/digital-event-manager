dd238641ee3408d41a9555acc461220ebc0e8e51
Ivan Rodin <learpcs@gmail.com>
2026-05-15 01:51:02 +0400

    fix tests

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,6 @@ from app.core.config import settings
 def create_app() -> FastAPI:
     application = FastAPI(title=settings.app_name)
     application.include_router(api_v1_router, prefix=settings.api_v1_prefix)
-    configure_openapi(application)
     return application
 
 
