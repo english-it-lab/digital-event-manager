d13444baef4808f9181fae18fc89840ba8eee87d
Guest838 <Guest838@yandex.ru>
2026-05-16 17:46:15 +0400

    final ruff

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,7 +4,6 @@ from app.adapters.api.v1 import router as api_v1_router
 from app.core.config import settings
 
 
-
 def create_app() -> FastAPI:
     application = FastAPI(title=settings.app_name)
     application.include_router(api_v1_router, prefix=settings.api_v1_prefix)
